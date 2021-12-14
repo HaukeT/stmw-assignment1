3cf571dd198563fecd9c67cf5b36a412c30e285e
Hauke <hauke2@uni-bremen.de>
2021-12-14 03:56:27 +0100

    xml pondering

--- MySAX.java
+++ MySAX.java
@@ -8,39 +8,39 @@
  *
  */
 
-import java.io.FileReader;
-import java.text.*;
-import java.util.*;
-import org.xml.sax.XMLReader;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
-import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import java.io.FileReader;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Locale;
 
 
-public class MySAX extends DefaultHandler
-{
+public class MySAX extends DefaultHandler {
 
-    public static void main (String args[])
-	throws Exception
-    {
-	XMLReader xr = XMLReaderFactory.createXMLReader();
-	MySAX handler = new MySAX();
-	xr.setContentHandler(handler);
-	xr.setErrorHandler(handler);
+    public static void main(String args[])
+            throws Exception {
+        XMLReader xr = XMLReaderFactory.createXMLReader();
+        MySAX handler = new MySAX();
+        xr.setContentHandler(handler);
+        xr.setErrorHandler(handler);
 
-				// Parse each file provided on the
-				// command line.
-	for (int i = 0; i < args.length; i++) {
-	    FileReader r = new FileReader(args[i]);
-	    xr.parse(new InputSource(r));
-	}
+        // Parse each file provided on the
+        // command line.
+        for (int i = 0; i < args.length; i++) {
+            FileReader r = new FileReader(args[i]);
+            xr.parse(new InputSource(r));
+        }
     }
 
 
-    public MySAX ()
-    {
-	super();
+    public MySAX() {
+        super();
     }
 
     /* Returns the amount (in XXXXX.xx format) denoted by a money-string
@@ -52,10 +52,11 @@ public class MySAX extends DefaultHandler
         else {
             double am = 0.0;
             NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
-            try { am = nf.parse(money).doubleValue(); }
-            catch (ParseException e) {
+            try {
+                am = nf.parse(money).doubleValue();
+            } catch (ParseException e) {
                 System.out.println("This method should work for all " +
-                                   "money values you find in our data.");
+                        "money values you find in our data.");
                 System.exit(20);
             }
             nf.setGroupingUsed(false);
@@ -67,67 +68,122 @@ public class MySAX extends DefaultHandler
     // Event handlers.
     ////////////////////////////////////////////////////////////////////
 
+    private String Characters = "";
+    HashMap<String, Integer> categories = new HashMap<String, Integer>();
 
-    public void startDocument ()
-    {
-	System.out.println("Start document");
+
+    public void startDocument() {
+        System.out.println("Start document");
     }
 
 
-    public void endDocument ()
-    {
-	System.out.println("End document");
+    public void endDocument() {
+        writeCategory();
+        System.out.println("End document");
     }
 
 
-    public void startElement (String uri, String name,
-			      String qName, Attributes atts)
-    {
-	if ("".equals (uri))
-	    System.out.println("Start element: " + qName);
-	else
-	    System.out.println("Start element: {" + uri + "}" + name);
-	for (int i = 0; i < atts.getLength(); i++) {
-	    System.out.println("Attribute: " + atts.getLocalName(i) + "=" + atts.getValue(i));
-	}
+    public void startElement(String uri, String name,
+                             String qName, Attributes atts) {
+        /*
+        if ("".equals(uri))
+            System.out.println("Start element: " + qName);
+        else
+            System.out.println("Start element: {" + uri + "}" + name);
+        for (int i = 0; i < atts.getLength(); i++) {
+            System.out.println("Attribute: " + atts.getLocalName(i) + "=" + atts.getValue(i));
+        }
+         */
+        Characters = "";
     }
 
 
-    public void endElement (String uri, String name, String qName)
-    {
-	if ("".equals (uri))
-	    System.out.println("End element: " + qName);
-	else
-	    System.out.println("End element:   {" + uri + "}" + name);
+    public void endElement(String uri, String name, String qName) {
+        /*
+        if ("".equals(uri))
+            System.out.println("End element: " + qName);
+        else
+            System.out.println("End element:   {" + uri + "}" + name);
+         */
+        if ("Category".equals(qName))
+            duplicateElimator();
+        Characters = "";
     }
 
 
-    public void characters (char ch[], int start, int length)
-    {
-	System.out.print("Characters:    \"");
-	for (int i = start; i < start + length; i++) {
-	    switch (ch[i]) {
-	    case '\\':
-		System.out.print("\\\\");
-		break;
-	    case '"':
-		System.out.print("\\\"");
-		break;
-	    case '\n':
-		System.out.print("\\n");
-		break;
-	    case '\r':
-		System.out.print("\\r");
-		break;
-	    case '\t':
-		System.out.print("\\t");
-		break;
-	    default:
-		System.out.print(ch[i]);
-		break;
-	    }
-	}
-	System.out.print("\"\n");
+    public void characters(char ch[], int start, int length) {
+        //System.out.print("Characters:    \"");
+        String normalString = new String(ch, start, length);
+        Characters += normalString;
+        /*
+        for (int i = start; i < start + length; i++) {
+            switch (ch[i]) {
+                case '\\':
+                    System.out.print("\\\\");
+                    break;
+                case '"':
+                    System.out.print("\\\"");
+                    break;
+                case '\n':
+                    System.out.print("\\n");
+                    break;
+                case '\r':
+                    System.out.print("\\r");
+                    break;
+                case '\t':
+                    System.out.print("\\t");
+                    break;
+                default:
+                    System.out.print(ch[i]);
+                    break;
+            }
+        }
+        System.out.print("\"\n");
+         */
     }
+
+    public void duplicateElimator() {
+        if (categories.containsKey(Characters)) {
+            return;
+        } else {
+            categories.put(Characters, categories.size() + 1);
+        }
+    }
+
+    public void writeCategory() {
+        categories.forEach((key, value) -> System.out.println(value + "," + csvEscape(key)));
+        System.out.println(categories.size());
+    }
+
+    public String csvEscape(String value) {
+
+
+        if (value.contains("\""))
+        {
+            return value.replaceAll("\"","\"\"");
+        }
+
+        if (value.contains("\r"))
+        {
+            return value.replaceAll("\r","\"\r\"");
+        }
+
+        if (value.contains("\n"))
+        {
+            return value.replaceAll("\n","\"\n\"");
+        }
+
+        if (value.equals(""))
+        {
+            return "";
+        }
+
+        if (value.contains(",")) {
+            return "\"" + value + "\"";
+        } else {
+            return value;
+        }
+    }
+
 
 }

d54b449ee2ce29337eec11948dd4881f87afac0d
STMW <hauke2@uni-bremen.de>
2021-12-19 15:15:04 +0100

    fix queries

--- queries.sql
+++ queries.sql
@@ -10,11 +10,11 @@ where Location IN (select LocationID L from Location where Name = 'New York');
 #3. 8365 done
 -- 8364, 1 item fehlt :(
 select count(ItemID)
-from (select * from Item_Categories group by ItemID having count(CategoryID) = 4) as `IC*`;
+from (select ItemID from Item_Categories group by ItemID having count(CategoryID) = 4) as `IC*`;
 
 
 #4 1046740686 done
-select ItemID, Currently
+select ItemID
 from Auction
 where Currently =
       (select max(Currently)
@@ -42,36 +42,3 @@ select count(distinct CategoryID) from Item_Categories where ItemID in
 (select ItemID from Auction where auctionID in
 (select Auction from Bid where Amount > 100));
 
--- ------------------
--- test queries
-
--- 19532
-select count(*)
-from Item;
-
--- 90269
-select count(*)
-from Item_Categories;
-
-select count(*)
-from Category;
-
--- 13129
-select count(distinct Seller) as seller_count
-from Auction;
-
--- 7010
-select count(distinct Bidder) as bidder_count
-from Bid;
-
--- 1959
-select count(*)
-from Auction
-where Buy_Price <> 0;
-
--- 13090
-select count(DISTINCT i.ItemID)
-from Item i
-         JOIN Location l ON i.Location = l.LocationID
-WHERE l.Latitude <> 0
-  AND l.Longitude <> 0;
